344daeeb82d27f91ee78ae21df60a57a843fd780
fidaamahboob <fidaa_786@hotmail.com>
2024-06-20 15:21:41 +0100

    Added skills section content and factored out hard coded content in to an JSON object for easy access.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,61 +2,34 @@ import { title } from "@/components/primitives";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, } from "@/components/ui/card";
 import { Section } from "@/components/ui/section";
+import { RESUME_DATA } from "@/data/resume-data";
 import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
 
 export default function ResumePage() {
 
-  const RESUME_DATA = {
-    work: [
-      {
-        company: "Parabol",
-        link: "https://parabol.co",
-        badges: ["Remote"],
-        title: "Senior Full Stack Developer",
-        start: "2021",
-        end: "2024",
-        description:
-          "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-      },
-      {
-        company: "Clevertech",
-        link: "https://clevertech.biz",
-        badges: ["Remote"],
-        title: "Lead Android Developer → Full Stack Developer",
-        start: "2015",
-        end: "2021",
-        description:
-          "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-      },
-      {
-        company: "Jojo Mobile",
-        link: "https://bsgroup.eu/",
-        badges: [],
-        title: "Android Developer → Lead Android Developer",
-        start: "2012",
-        end: "2015",
-        description:
-          "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-      }]
-  }
   return (
     <>
       <div className="flex items-center justify-between bg-red-600">
         <div className="flex-1 space-y-1.5">
           <div>
-            <h1 className="text-4xl">Fidaa Mahboob</h1>
+            <h1 className="text-4xl">{RESUME_DATA.name}</h1>
           </div>
           <div>
-            <h2>Full Stack Software Engineer</h2>
+            <h2>{RESUME_DATA.title}</h2>
           </div>
         </div>
       </div>
       <Section>
         <h2 className="text-xl font-bold">About</h2>
-        <p className="text-pretty text-sm text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className="text-pretty text-sm text-muted-foreground">{RESUME_DATA.about}</p>
       </Section>
       <Section>
         <h2 className="text-xl font-bold">Skills</h2>
+        <div className="flex flex-wrap gap-1">
+          {RESUME_DATA.skills.map((skill) => {
+            return <Badge key={skill}>{skill}</Badge>;
+          })}
+        </div>
       </Section>
       <Section>
         <h2 className="text-xl font-bold">Experience</h2>
