736f3696032f888d894c8677aef95f11e2197634
fidaamahboob <fidaa_786@hotmail.com>
2024-06-20 15:35:23 +0100

    Added ui components for resume page and changed dummy data to factual bio info.

---
+++ data/resume-data.tsx
@@ -0,0 +1,40 @@
+export const RESUME_DATA = {
+  name: "Fidaa Mahboob",
+  title: "Full Stack Software Engineer",
+  about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  skills: ["React", "Javascript", "Java", "Terraform", "AWS"],
+  work: [
+    {
+      company: "AWS Cloud Resume Challenge",
+      link: "https://cloudresumechallenge.dev/docs/the-challenge/",
+      badges: [],
+      title: "Participant",
+      start: "Jun 2023",
+      end: "Current",
+      description:
+        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+    },
+    {
+      company: "Accenture",
+      link: "https://www.accenture.com/gb-en",
+      badges: ["Remote"],
+      title: "Full Stack Engineering Senior Analyst",
+      start: "Dec 2022",
+      end: "Nov 2023",
+      description:
+        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+    },
+    {
+      company: "Accenture",
+      link: "https://www.accenture.com/gb-en",
+      badges: ["Remote"],
+      title: "Full Stack Engineering Analyst",
+      start: "Mar 2022",
+      end: "Nov 2022",
+      description:
+        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+    }],
+  locationLink: "",
+  location: "Dewsbury, West Yorkshire, UK",
+
+} as const
