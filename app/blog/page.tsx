1acc8f60ee2f1c84ffaef230f45e40217d1f71c2
fidaamahboob <fidaa_786@hotmail.com>
2024-07-08 11:24:39 +0100

    Modified display of meta data subtitle about blog post.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,23 +2,22 @@
 
 import { Card, CardHeader } from "@/components/ui/card";
 import { Section } from "@/components/ui/section";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 
 export default function BlogPage() {
 
-  const [blog, setBlog] = useState<any[]>([]);  
+  const [blog, setBlog] = useState<any[]>([]);
 
   const getData = async () => {
     const data = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@fidaamahboob/");
     const json = await data.json();
     const result = await json.items;
     setBlog(result)
-    console.log(result);
   };
 
-  const convertDateFormat = (date: string) : React.ReactNode => {
+  const convertDateFormat = (date: string): React.ReactNode => {
     const convertedDate = new Date(date);
-    return <p className="font-semibold">Published: &nbsp; {convertedDate.toString().slice(0, 15)}</p>; 
+    return <p className="italic">Published by Fidaa Mahboob on {convertedDate.toString().slice(0, 15)}</p>;
   }
 
   useEffect(() => {
@@ -29,25 +28,25 @@ export default function BlogPage() {
     <div>
       <h1 className="text-3xl font-bold">Blog</h1>
       <Section>
-        {blog.map(item => (
-          <Card key={item.link} className="py-4">
-            <CardHeader>
-              <div className="flex items-center justify-between gap-x-1 text-base">
-                <h3 className="inline-flex items-center justify-center text-pretty gap-x-1 text-xl font-bold leading-none">
-                  <a target="_blank" href={item.link}>
-                    {item.title}
-                  </a>
-                </h3>
-              </div>
-              <div>
-                {convertDateFormat(item.pubDate)}
-              </div>
-              <h4 className="font-mono text-md leading-none">
-                by {item.author}
-              </h4>
-            </CardHeader>
-          </Card>
-        ))}
+        <Suspense fallback={<p className="text-xl text-black-400">Loading Blog post feed......</p>}>
+          {blog.map(item => (
+            <Card key={item.link} className="py-4">
+              <CardHeader>
+                <div className="flex items-center justify-between gap-x-1 text-base">
+                  <h3 className="inline-flex items-center justify-center text-pretty gap-x-1 text-xl font-bold leading-none">
+                    <a target="_blank" href={item.link}>
+                      {item.title}
+                    </a>
+                  </h3>
+                </div>
+                <div>
+                  {convertDateFormat(item.pubDate)}
+                </div>
+              </CardHeader>
+            </Card>
+          ))}
+        </Suspense>
+
       </Section>
     </div>
   );
